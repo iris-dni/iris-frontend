d992b0f91935de07008153143425e401d8880313
Matt Berridge <hi@builtbyboon.com>
2016-07-07 18:32:47 +0200

    Adds webpack dev server config \o/

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route, Router } from 'react-router';
+import { Route, Router, browserHistory } from 'react-router';
 
 import Home from 'views/Home';
 import Basic from 'views/Basic';
 
 export default (
-  <Router>
+  <Router history={browserHistory}>
     <Route path='/' component={Home} />
     <Route path='/basic' component={Basic} />
   </Router>
---
+++ webpack.config.dev.js
@@ -0,0 +1,52 @@
+var webpack = require('webpack');
+var config = require('./webpack.config.prod.js');
+
+config.cache = true;
+config.debug = true;
+config.devtool = 'eval';
+
+config.entry.unshift(
+  'webpack-dev-server/client?http://localhost:8080',
+  'webpack/hot/only-dev-server'
+);
+
+config.output.publicPath = 'http://localhost:8080/dist/';
+config.output.hotUpdateMainFilename = 'update/[hash]/update.json';
+config.output.hotUpdateChunkFilename = 'update/[hash]/[id].update.js';
+
+config.plugins = [
+  new webpack.DefinePlugin({
+    __CLIENT__: true,
+    __SERVER__: false
+  }),
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin()
+];
+
+config.module = {
+  loaders: [
+    {
+      include: /\.json$/,
+      loaders: ['json-loader']
+    },
+    {
+      include: /\.(jsx?)$/,
+      loaders: ['react-hot', 'babel'], exclude: /node_modules/
+    }
+  ]
+};
+
+config.devServer = {
+  publicPath: 'http://localhost:8080/dist/',
+  contentBase: './static',
+  hot: true,
+  inline: true,
+  lazy: false,
+  quiet: true,
+  noInfo: false,
+  headers: { 'Access-Control-Allow-Origin': '*' },
+  stats: { colors: true },
+  host: '0.0.0.0'
+};
+
+module.exports = config;
