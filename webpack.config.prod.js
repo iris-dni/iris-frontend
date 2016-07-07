6d530f49a5de890aac1eddc821fbd01230d43384
Matt Berridge <hi@builtbyboon.com>
2016-07-07 18:20:48 +0200

    Adds webpack build for prod

---
+++ webpack.config.prod.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: ['./src/client'],
+  target: 'web',
+  cache: false,
+  context: __dirname,
+  devtool: false,
+  output: {
+    path: path.join(__dirname, 'static/dist'),
+    filename: 'client.js',
+    chunkFilename: '[name].[id].js',
+    publicPath: 'dist/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __CLIENT__: true,
+      __SERVER__: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production'
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        include: /\.json$/,
+        loaders: ['json-loader']
+      },
+      {
+        include: /\.(jsx?)$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modulesDirectories: [
+      'src',
+      'node_modules',
+      'web_modules'
+    ],
+    extensions: ['', '.json', '.js', '.jsx']
+  },
+  node: {
+    __dirname: true,
+    fs: 'empty'
+  }
+};
