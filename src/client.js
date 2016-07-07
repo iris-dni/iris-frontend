faf159d8cecf799c1892ac34758f988f08801eb1
Matt Berridge <hi@builtbyboon.com>
2016-07-07 18:10:00 +0200

    Updates App reference

--- src/client.js
+++ src/client.js
@@ -14,7 +14,7 @@ ReactDOM.render(
  * Detect whether the server-side render has been discarded due to an invalid checksum.
  */
 if (process.env.NODE_ENV !== 'production') {
-  const reactRoot = window.document.getElementById('react-root');
+  const reactRoot = window.document.getElementById('app');
 
   if (!reactRoot || !reactRoot.firstChild || !reactRoot.firstChild.attributes ||
       !reactRoot.firstChild.attributes['data-react-checksum']) {
