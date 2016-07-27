dfd95f492881e382e37ff7c7d0797d87ffdda3af
Matt Berridge <hi@builtbyboon.com>
2016-07-27 12:52:42 +0200

    Adds open source CSS basics with sample 'styleguide' page

---
+++ src/views/Styleguide.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PageMain from 'components/PageMain';
+import PageContent from 'components/PageContent';
+import PageSidebar from 'components/PageSidebar';
+import Container from 'components/Container';
+import Heading1 from 'components/Heading1';
+import Heading2 from 'components/Heading2';
+import Paragraph from 'components/Paragraph';
+
+export default (props) => (
+  <div>
+    <Container>
+      <Heading1 text={'Initiativ für bezahlbare Wohnungen'} />
+      <PageMain>
+        <PageContent>
+          <Heading2 text={'Begründung'} />
+          <Paragraph>
+            {'Der aktuelle Zustand ist nicht akzeptabel. Es gibt keinen sicheren Ort für unsere Kinder in den Park zu spielen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod velit et viverra feugiat. Morbi finibus eros non elementum tincidunt. Suspendisse in nisl sed lectus molestie lacinia. In sit amet diam sed eros blandit tristique sed quis sapien. Quisque fermentum lobortis nibh sit amet cursus.'}'
+          </Paragraph>
+          <Heading2 text={'Lösung'} />
+          <Paragraph>
+            {'Der aktuelle Zustand ist nicht akzeptabel. Es gibt keinen sicheren Ort für unsere Kinder in den Park zu spielen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod velit et viverra feugiat. Morbi finibus eros non elementum tincidunt. Suspendisse in nisl sed lectus molestie lacinia. In sit amet diam sed eros blandit tristique sed quis sapien. Quisque fermentum lobortis nibh sit amet cursus.'}'
+          </Paragraph>
+        </PageContent>
+        <PageSidebar>
+          <Paragraph>
+            {'Der aktuelle Zustand ist nicht akzeptabel. Es gibt keinen sicheren Ort für unsere Kinder in den Park zu spielen. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod velit et viverra feugiat. Morbi finibus eros non elementum tincidunt. Suspendisse in nisl sed lectus molestie lacinia. In sit amet diam sed eros blandit tristique sed quis sapien. Quisque fermentum lobortis nibh sit amet cursus.'}'
+          </Paragraph>
+        </PageSidebar>
+      </PageMain>
+    </Container>
+  </div>
+);
