29b26b71819da25d69cae9f84d1eb2b6262266b1
Matt Berridge <hi@builtbyboon.com>
2016-07-29 11:23:18 +0200

    Update createApiUrl helper

--- src/helpers/createApiUrl.js
+++ src/helpers/createApiUrl.js
@@ -1,8 +1,8 @@
 export default (API_URL = '', requestPath = '') => {
   // Remove trailing slash if given
   const sanitizedApiUrl = API_URL.replace(/\/$/, '');
-  // Make sure request path has starting slash
+  // Remove starting starting slash from request path
   const sanitizedRequestPath = requestPath.replace(/^\//, '');
-  // Return joined URL
+  // Return URL joined with a slash
   return [sanitizedApiUrl, sanitizedRequestPath].join('/');
 };
