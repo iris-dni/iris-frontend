9fa9ed716dcd2e6007987cc2cb46e2cbc7f9044e
Matt Berridge <hi@builtbyboon.com>
2016-07-29 10:56:59 +0200

    Adds path sanitizing for API url construction

--- src/helpers/createApiUrl.js
+++ src/helpers/createApiUrl.js
@@ -1,3 +1,8 @@
-export default (API_URL, requestPath) => {
-  return API_URL + requestPath;
+export default (API_URL = '', requestPath) => {
+  // Remove trailing slash if given
+  const sanitizedApiUrl = API_URL.replace(/\/$/, '');
+  // Make sure request path has starting slash
+  const sanitizedRequestPath = requestPath.replace(/^\//, '');
+  // Return joined URL
+  return [sanitizedApiUrl, sanitizedRequestPath].join('/');
 };
