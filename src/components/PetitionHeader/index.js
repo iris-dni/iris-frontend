916fa52fc9be6343479847156ab5c4499beb38df
Matt Berridge <hi@builtbyboon.com>
2016-08-02 17:39:21 +0200

    Adds theming architecture via sass maps

--- src/components/PetitionHeader/index.js
+++ src/components/PetitionHeader/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './petition-header.scss';
 import Heading1 from 'components/Heading1';
 
 const PetitionHeader = ({ title }) => (
-  <header>
-    <Heading1 text={title} />
+  <header className={styles.root}>
+    <div className={styles['heading-wrap']}>
+      <Heading1 text={title} />
+    </div>
   </header>
 );
 
