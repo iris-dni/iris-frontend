011b0c05c637b8af383a7a1b29f4161d97ecee98
Nico Durstewitz <nico.durstewitz@gmail.com>
2016-08-10 10:26:52 +0200

    Add headline "Petitionen" to petitions page

--- src/components/Petitions/index.js
+++ src/components/Petitions/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import PetitionListItem from 'components/PetitionListItem';
 
 const Petitions = ({ total, petitions, currentPage, perPage }) => (
-  <ul>
-    {petitions.map((petition) => <PetitionListItem {...petition} key={petition.id} />)}
-  </ul>
+  <div>
+    <h1>Petitionen</h1>
+    <ul>
+      {petitions.map((petition) => <PetitionListItem {...petition} key={petition.id} />)}
+    </ul>
+  </div>
 );
 
 export default Petitions;
