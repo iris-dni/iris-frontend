4f4e90a62f70c7320407e33300bacb8943df9bff
Nico Durstewitz <nico.durstewitz@gmail.com>
2016-08-11 12:44:19 +0200

    Redirect to /auth/login if not signed in

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import { withRouter } from 'react-router';
+import isSignedIn from './isSignedIn';
 
 const Auth = (WrappedComponent) => withRouter(React.createClass({
 
+  getInitialState: () => ({
+    me: null,
+    finishedLoading: false
+  }),
+
+  componentWillMount () {
+    // @TODO:
+    // 1. stash current path for redirecting later
+    // 2. Call whoami API endpoint to fetch current user
+    // 3. Set state with current user data
+    this.setState({
+      me: null,
+      finishedLoading: true
+    });
+  },
+
+  showLoginPage () {
+    setTimeout(() => {
+      this.props.router.push('/auth/login');
+    }, 6);
+  },
+
   render () {
-    if (true) {
-      return (
-        <WrappedComponent {...this.props} />
-      );
+    if (this.state.finishedLoading) {
+      if (isSignedIn(this.state.me)) {
+        return (
+          <WrappedComponent {...this.props} />
+        );
+      } else {
+        this.showLoginPage();
+      }
     }
+    return <div />;
   }
 }));
 
