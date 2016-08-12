a720a709d26a3c8e9c77da12e286590a0dc2b66b
Richard Bausek <r.bausek@de.edenspiekermann.com>
2016-08-12 16:55:15 +0200

    add CircleProgressBar component

---
+++ src/components/CircleProgressBar/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './circleprogressbar.scss';
+import ProgressBar from 'progressbar.js';
+
+const CircleProgressBar = React.createClass({
+
+  getDefaultProps () {
+    return {
+      percentage: 0,
+      animated: false,
+      size: 'small'
+    };
+  },
+
+  componentDidMount () {
+    this.initProgressBar();
+    this.updateProgressBar(this.props.percentage);
+  },
+
+  initProgressBar () {
+    let canvasElement = ReactDOM.findDOMNode(this.refs.canvas);
+
+    this.progressBar = new ProgressBar.Circle(canvasElement, {
+      strokeWidth: 10,
+      easing: 'easeInOut',
+      duration: 800,
+      color: '#2fcd0c',
+      trailColor: '#f1f1f1',
+      trailWidth: 10,
+      svgStyle: {
+        width: '100%',
+        display: 'block',
+        strokeLinecap: 'round'
+      }
+    });
+  },
+
+  updateProgressBar (percentage) {
+    let progress = percentage / 100;
+
+    if (this.props.animated) {
+      this.progressBar.animate(progress);
+    } else {
+      this.progressBar.set(progress);
+    }
+  },
+
+  render () {
+    return (
+      <div className={styles.root}>
+        <div className={styles[this.props.size]} ref='canvas'></div>
+        <div className={styles.content}>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+});
+
+export default CircleProgressBar;
+
