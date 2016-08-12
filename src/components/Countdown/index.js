97fd662948ef53890f7458b192122248fd06199e
Richard Bausek <r.bausek@de.edenspiekermann.com>
2016-08-12 19:00:41 +0200

    update Countdown component to use CircleProgressBar

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './countdown.scss';
 import settings from 'settings';
+import CircleProgressBar from 'components/CircleProgressBar';
 
 const Countdown = ({ remaining }) => (
-  <p className={styles.root}>
-    <span className={styles.counter}>
-      <strong className={styles.total}>{remaining || 0}</strong> <span className={styles.text}>{settings.daysRemaining}</span>
-    </span>
-  </p>
+  <div className={styles.root}>
+    <CircleProgressBar percentage={20} size='large' animated>
+      <span className={styles.counter}>
+        <strong className={styles.total}>{remaining || 0}</strong> <span className={styles.text}>{settings.daysRemaining}</span>
+      </span>
+    </CircleProgressBar>
+  </div>
 );
 
 export default Countdown;
