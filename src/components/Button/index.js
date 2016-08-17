07c065cc329e51e68a151fb6339f8b61aa96f50f
Matt Berridge <hi@builtbyboon.com>
2016-08-17 14:48:45 +0200

    Remove console log

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import styles from './button.scss';
 
-const Button = ({ children, text, type, modifier, disabled }) => {
-  console.log('MODDED', modifier);
-  return (
-    <button type={type || 'submit'} className={disabled ? styles.disabled : styles[modifier || 'default']} disabled={disabled}>
-      {children || text}
-    </button>
-  );
-};
+const Button = ({ children, text, type, modifier, disabled }) => (
+  <button type={type || 'submit'} className={disabled ? styles.disabled : styles[modifier || 'default']} disabled={disabled}>
+    {children || text}
+  </button>
+);
 
 export default Button;
