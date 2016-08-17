76f8851d2511cc71f7404ff4afb9e3cc2b8e7475
Matt Berridge <hi@builtbyboon.com>
2016-08-17 16:22:17 +0200

    Adds test for fieldValidator

--- src/form/fieldValidator.js
+++ src/form/fieldValidator.js
@@ -3,16 +3,20 @@ import settings from 'settings';
 export default (fields, values) => {
   const errors = {};
 
-  fields.map(field => {
+  fields && fields.map(field => {
+    if (!field) {
+      return;
+    }
+
     const key = field.name;
     const isRequired = field.html && field.html.required;
     const minLength = field.html && field.html.minLength;
     const maxLength = field.html && field.html.maxLength;
 
-    if (isRequired && !values[key]) {
+    if (isRequired && values && !values[key]) {
       // test for required fields
       errors[key] = settings.requiredText;
-    } else if (minLength && values[key] && values[key].length > maxLength) {
+    } else if (maxLength && values[key] && values[key].length > maxLength) {
       // test against maxLength
       errors[key] = settings.maxLengthText.replace('%x', maxLength);
     } else if (minLength && values[key] && values[key].length < minLength) {
---
+++ test/form/fieldValidator.js
@@ -0,0 +1,135 @@
+import chai from 'chai';
+import fieldValidator from 'form/fieldValidator';
+
+const { assert } = chai;
+
+describe('fieldValidator', () => {
+  it('returns empty errors object when field is not required', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'test'
+        }
+      ],
+      { test: '' }
+    );
+    const expected = {};
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('returns correct error when field is required', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'test',
+          html: {
+            required: true
+          }
+        }
+      ],
+      { test: '' }
+    );
+    const expected = {
+      test: 'This field is required'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('returns correct error when minLength is not met', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'test',
+          html: {
+            minLength: 10
+          }
+        }
+      ],
+      { test: 'test' }
+    );
+    const expected = {
+      test: 'Must be 10 characters or more'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('returns correct error when maxLength exceeds limit', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'test',
+          html: {
+            maxLength: 10
+          }
+        }
+      ],
+      { test: 'Words exceeding the limit' }
+    );
+    const expected = {
+      test: 'Cannot be more than 10 characters'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('handles multiple field errors', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'foo',
+          html: {
+            maxLength: 10
+          }
+        },
+        {
+          name: 'bar',
+          html: {
+            required: true
+          }
+        }
+      ],
+      {
+        foo: 'Words exceeding the limit',
+        bar: ''
+      }
+    );
+    const expected = {
+      foo: 'Cannot be more than 10 characters',
+      bar: 'This field is required'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('prioritizes "required field" error', () => {
+    const actual = fieldValidator(
+      [
+        {
+          name: 'test',
+          html: {
+            required: true,
+            minLength: 10
+          }
+        }
+      ],
+      {
+        test: ''
+      }
+    );
+    const expected = {
+      test: 'This field is required'
+    };
+
+    assert.deepEqual(actual, expected);
+  });
+
+  it('fails silently', () => {
+    const actual = fieldValidator();
+    const expected = {};
+
+    assert.deepEqual(actual, expected);
+  });
+});
