16b597c5a9477a92dd5dcf7a3d300f3cb6035bd1
Richard Bausek <r.bausek@de.edenspiekermann.com>
2016-08-17 18:20:07 +0200

    hide city on petition detail if not set

--- src/components/PetitionInfo/index.js
+++ src/components/PetitionInfo/index.js
@@ -4,12 +4,14 @@ import IconAndInfo from 'components/IconAndInfo';
 
 const PetitionInfo = ({ city, dateRange }) => (
   <ul className={styles.root}>
-    <li className={styles.item}>
-      <IconAndInfo
-        icon='Pin'
-        info={city}
-      />
-    </li>
+    {city &&
+      <li className={styles.item}>
+        <IconAndInfo
+          icon='Pin'
+          info={city}
+        />
+      </li>
+    }
     <li className={styles.item}>
       <IconAndInfo
         icon='Clock'
