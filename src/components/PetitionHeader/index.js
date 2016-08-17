beafb7dfe6a1668b65c35d8d36f076a9df60b920
Richard Bausek <r.bausek@de.edenspiekermann.com>
2016-08-17 18:19:39 +0200

    replace Progressbar with same logic as CircleProgressBar and hide supporters progress if no required supporters

--- src/components/PetitionHeader/index.js
+++ src/components/PetitionHeader/index.js
@@ -5,18 +5,23 @@ import PetitionInfo from 'components/PetitionInfo';
 import PetitionStats from 'components/PetitionStats';
 import ProgressBar from 'components/ProgressBar';
 
-const PetitionHeader = ({ title, info, supporters, percentComplete }) => (
+const PetitionHeader = ({ title, info, metrics }) => (
   <header className={styles.root}>
     <div className={styles.heading}>
       <Heading1 text={title} />
     </div>
     <div className={styles.info}>
       <PetitionInfo {...info} />
-      <ProgressBar
-        animate
-        percentage={percentComplete}
-      />
-      <PetitionStats {...supporters} />
+      {metrics.supportersMetric.votingActive &&
+        <div className={styles.progress}>
+          <ProgressBar
+            animated
+            percentage={metrics.supportersMetric.percentage}
+            aria={metrics.supportersMetric.aria}
+          />
+          <PetitionStats {...metrics.supportersMetric} />
+        </div>
+      }
     </div>
   </header>
 );
--- src/components/ProgressBar/index.js
+++ src/components/ProgressBar/index.js
@@ -1,31 +1,62 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import styles from './progress-bar.scss';
+import settings from 'settings';
+import ProgressBarJS from 'progressbar.js';
 
 const ProgressBar = React.createClass({
+
+  getDefaultProps: () => ({
+    percentage: 0,
+    animated: false,
+    color: settings.colors.primary,
+    trailColor: settings.colors.greyLite,
+    aria: {}
+  }),
+
   componentDidMount () {
-    if (this.props.animate) {
-      setTimeout(() => this.setState({
-        loading: false }),
-      150);
+    this.initProgressBar();
+    this.updateProgressBar(this.props.percentage);
+  },
+
+  initProgressBar () {
+    const canvasElement = ReactDOM.findDOMNode(this.refs.canvas);
+
+    this.progressBar = new ProgressBarJS.Line(canvasElement, {
+      strokeWidth: 4,
+      easing: 'easeInOut',
+      duration: 1500,
+      color: this.props.color,
+      trailColor: this.props.trailColor,
+      trailWidth: 4,
+      svgStyle: {
+        width: '100%',
+        height: '100%'
+      },
+      autoStyleContainer: false
+    });
+  },
+
+  updateProgressBar (percentage) {
+    const progress = percentage / 100;
+
+    if (this.props.animated) {
+      this.progressBar.animate(progress);
+    } else {
+      this.progressBar.set(progress);
     }
   },
 
-  getInitialState: () => ({
-    loading: true
-  }),
-
   render () {
-    const appliedClass = this.state.loading
-      ? styles.loading
-      : styles.finished;
-
     return (
-      <span aria-hidden className={styles.root}>
-        <span
-          className={appliedClass}
-          style={{ width: `${this.props.percentage}%` }}
-        />
-      </span>
+      <div
+        className={styles.root}
+        ref='canvas'
+        role='progressbar'
+        aria-valuenow={this.props.aria.value}
+        aria-valuemin={this.props.aria.minimum}
+        aria-valuemax={this.props.aria.maximum}
+      />
     );
   }
 });
