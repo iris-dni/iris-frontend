325c5edbf907d6f424c5caeac8a4e831ec20cb54
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2016-08-18 11:05:44 +0200

    Restucture tests for PetitionActions

--- test/actions/PetitionActions.js
+++ test/actions/PetitionActions.js
@@ -14,75 +14,87 @@ import {
 const { assert } = chai;
 
 describe('PetitionActions', () => {
-  it('requestPetition returns REQUEST_PETITION action', () => {
-    const result = requestPetition();
-    const actual = result.type;
-    const expected = 'REQUEST_PETITION';
+  describe('requestPetition', () => {
+    it('returns REQUEST_PETITION action', () => {
+      const result = requestPetition();
+      const actual = result.type;
+      const expected = 'REQUEST_PETITION';
 
-    assert.equal(actual, expected);
+      assert.equal(actual, expected);
+    });
   });
 
-  it('receivePetition returns RECEIVE_PETITION action', () => {
-    const result = receivePetition();
-    const actual = result.type;
-    const expected = 'RECEIVE_PETITION';
+  describe('receivePetition', () => {
+    it('returns RECEIVE_PETITION action', () => {
+      const result = receivePetition();
+      const actual = result.type;
+      const expected = 'RECEIVE_PETITION';
 
-    assert.equal(actual, expected);
+      assert.equal(actual, expected);
+    });
+
+    it('passes petition object', () => {
+      const result = receivePetition(mockPetition);
+      const actual = result.petition;
+      const expected = mockPetition;
+
+      assert.deepEqual(actual, expected);
+    });
   });
 
-  it('receivePetition passes petition object', () => {
-    const result = receivePetition(mockPetition);
-    const actual = result.petition;
-    const expected = mockPetition;
+  describe('requestPetitions', () => {
+    it('returns REQUEST_PETITIONS action', () => {
+      const result = requestPetitions();
+      const actual = result.type;
+      const expected = 'REQUEST_PETITIONS';
 
-    assert.deepEqual(actual, expected);
+      assert.equal(actual, expected);
+    });
   });
 
-  it('requestPetitions returns REQUEST_PETITIONS action', () => {
-    const result = requestPetitions();
-    const actual = result.type;
-    const expected = 'REQUEST_PETITIONS';
+  describe('receivePetitions', () => {
+    it('returns RECEIVE_PETITIONS action', () => {
+      const result = receivePetitions();
+      const actual = result.type;
+      const expected = 'RECEIVE_PETITIONS';
 
-    assert.equal(actual, expected);
+      assert.equal(actual, expected);
+    });
+
+    it('passes petitions object', () => {
+      const result = receivePetitions(mockPetitions);
+      const actual = result.petitions;
+      const expected = mockPetitions;
+
+      assert.deepEqual(actual, expected);
+    });
   });
 
-  it('receivePetitions returns RECEIVE_PETITIONS action', () => {
-    const result = receivePetitions();
-    const actual = result.type;
-    const expected = 'RECEIVE_PETITIONS';
+  describe('submitPetition', () => {
+    it('returns SUBMIT_PETITION action', () => {
+      const result = submitPetition();
+      const actual = result.type;
+      const expected = 'SUBMIT_PETITION';
 
-    assert.equal(actual, expected);
+      assert.equal(actual, expected);
+    });
   });
 
-  it('receivePetitions passes petitions object', () => {
-    const result = receivePetitions(mockPetitions);
-    const actual = result.petitions;
-    const expected = mockPetitions;
+  describe('createdPetition', () => {
+    it('returns CREATED_PETITION action', () => {
+      const result = createdPetition();
+      const actual = result.type;
+      const expected = 'CREATED_PETITION';
 
-    assert.deepEqual(actual, expected);
-  });
+      assert.equal(actual, expected);
+    });
 
-  it('submitPetition returns SUBMIT_PETITION action', () => {
-    const result = submitPetition();
-    const actual = result.type;
-    const expected = 'SUBMIT_PETITION';
+    it('passes petition ID', () => {
+      const result = createdPetition(23);
+      const actual = result.id;
+      const expected = 23;
 
-    assert.equal(actual, expected);
-  });
-
-  it('createdPetition returns CREATED_PETITION action', () => {
-    const result = createdPetition();
-    const actual = result.type;
-    const expected = 'CREATED_PETITION';
-
-    assert.equal(actual, expected);
-  });
-
-  it('createdPetition passes petition ID', () => {
-    const result = createdPetition(23);
-    const actual = result.id;
-    const expected = 23;
-
-    assert.equal(actual, expected);
+      assert.equal(actual, expected);
+    });
   });
 });
