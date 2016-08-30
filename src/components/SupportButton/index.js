dc28ce030380f7acc8b332d5bd2a4543c65eca31
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2016-08-30 16:04:05 +0200

    Disable support button if petition is not supportable

--- src/components/SupportButton/index.js
+++ src/components/SupportButton/index.js
@@ -3,21 +3,31 @@ import { connect } from 'react-redux';
 import { supportPetition } from 'actions/PetitionActions';
 import Button from 'components/Button';
 import settings from 'settings';
+import getPetionSupportable from 'selectors/petitionSupportable';
 
-const SupportButton = ({ petition, supportPetition }) => (
+const SupportButton = ({ petition, supportable, supportPetition }) => (
   <Button
     type='button'
     onClick={() => supportPetition(petition)}
     size={'smaller'}
     modifier={'accent'}
+    disabled={!supportable}
     text={settings.petition.supportButton} />
 );
 
-export const mapStateToProps = ({ petition }) => ({ petition });
+const mapStateToProps = ({ petition }) => ({
+  petition,
+  supportable: getPetionSupportable(petition)
+});
 
-export const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = (dispatch) => ({
   supportPetition: (petition) => dispatch(supportPetition(petition))
 });
+
+SupportButton.propTypes = {
+  petition: React.PropTypes.object,
+  supportable: React.PropTypes.bool
+};
 
 export default connect(
   mapStateToProps,
