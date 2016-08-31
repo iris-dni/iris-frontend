ac7c094ca7a5b6a2f0d822a986eaadf0205f7d3b
Matt Berridge <hi@builtbyboon.com>
2016-08-31 13:16:46 +0200

    Adds external / internal switch for ButtonLink

--- src/components/ButtonLink/index.js
+++ src/components/ButtonLink/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router';
 import styles from '../Button/button.scss';
 
-const ButtonLink = ({ children, text, href, modifier, onClick }) => (
-  <a
-    href={href || '#'}
-    onClick={onClick ? (e) => { e.preventDefault(); onClick(); } : () => {}}
-    className={styles[modifier || 'default']}>
-    {children || text}
-  </a>
+const createAttrs = (props) => {
+  return {
+    [props.external ? 'href' : 'to']: props.href || '#',
+    onClick: props.onClick
+      ? (e) => { e.preventDefault(); props.onClick(); }
+      : () => {},
+    className: styles[props.modifier || 'default']
+  };
+};
+
+const ButtonLink = (props) => (
+  props.external
+    ? <a {...createAttrs(props)}>{props.children || props.text}</a>
+    : <Link {...createAttrs(props)}>{props.children || props.text}</Link>
 );
 
 export default ButtonLink;
--- src/components/SsoLink/index.js
+++ src/components/SsoLink/index.js
@@ -4,6 +4,7 @@ import ButtonLink from 'components/ButtonLink';
 const SsoLink = ({ text, url }) => {
   return (
     <ButtonLink
+      external
       text={text}
       href={url}
       modifier={'accent'}
