eca8e8062da1efbe7daa72002ca833f89f62e576
Tom Bonnike <bonniketom@gmail.com>
2016-09-01 12:19:03 +0200

    Added “(optional)“ tip to form labels

--- src/components/FormLabel/index.js
+++ src/components/FormLabel/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './form-label.scss';
 
-export default ({ fieldId, label, hint }) => (
+export default ({ fieldId, label, hint, required }) => (
   <div className={styles.root}>
-    <label htmlFor={fieldId} className={styles.label}>{label}</label>
+    <label htmlFor={fieldId} className={styles.label}>
+      {label} {!required && '(optional)'}
+    </label>
     {hint &&
       <em className={styles.hint}>{hint}</em>
     }
