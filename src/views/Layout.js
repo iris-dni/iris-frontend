66fa5c7efd496f25ac7945a5017480395287b600
Matt Berridge <hi@builtbyboon.com>
2016-09-01 12:18:57 +0200

    Make Layout component pure

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -5,24 +5,19 @@ import FlashMessage from 'containers/FlashMessage';
 import ModalWindow from 'containers/ModalWindow';
 import { TITLE_TEMPLATE } from 'server/getBrowserTitle';
 
-export default React.createClass({
-  render () {
-    const { children, flashMessage, modalWindow } = this.props;
-    return (
-      <div className={'wrapper'}>
-        <Helmet titleTemplate={TITLE_TEMPLATE} />
-        {flashMessage && flashMessage.text &&
-          <FlashMessage {...flashMessage} />
-        }
-        <main role='main'
-          aria-label='Content'
-          aria-hidden={(modalWindow && modalWindow.active) || false}>
-          {children}
-        </main>
-        {modalWindow && modalWindow.active &&
-          <ModalWindow {...modalWindow} />
-        }
-      </div>
-    );
-  }
-});
+export default ({ children, flashMessage, modalWindow }) => (
+  <div className={'wrapper'}>
+    <Helmet titleTemplate={TITLE_TEMPLATE} />
+    {flashMessage && flashMessage.text &&
+      <FlashMessage {...flashMessage} />
+    }
+    <main role='main'
+      aria-label='Content'
+      aria-hidden={(modalWindow && modalWindow.active) || false}>
+      {children}
+    </main>
+    {modalWindow && modalWindow.active &&
+      <ModalWindow {...modalWindow} />
+    }
+  </div>
+);
