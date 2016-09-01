597a02304c70d0459b4492174fbe10e2b82d309c
Tom Bonnike <bonniketom@gmail.com>
2016-09-01 15:25:52 +0200

    Refactored autocomplete component to use React class

--- src/components/Autocomplete/index.js
+++ src/components/Autocomplete/index.js
@@ -6,119 +6,117 @@ import styles from './autocomplete.scss';
 
 const MIN_QUERY_LENGTH = 2;
 
-const getClassname = (element, error) => {
-  return [
-    styles[element || 'input'],
-    styles[error ? 'invalid' : 'valid']
-  ].join(' ');
-};
+const Autocomplete = React.createClass({
+  getClassname (element, error) {
+    return [
+      styles[element || 'input'],
+      styles[error ? 'invalid' : 'valid']
+    ].join(' ');
+  },
 
-// We handle the query length validation here as we don‘t want to do it in the
-// (throttled) typeahead search. If we did, the first input after
-// MIN_QUERY_LENGTH would be delayed.
-const handleFetchRequest = (endpoint, e, typeaheadSearch, clearSuggestions) => {
-  let query = e.value;
+  handleFetchRequest (e, { endpoint, typeaheadSearch, clearSuggestions }) {
+    // We handle the query length validation here as we don‘t want to do it in
+    // the (throttled) typeahead search. If we did, the first input after
+    // MIN_QUERY_LENGTH would be delayed.
+    let query = e.value;
 
-  if (query) {
-    query = query.trim().toLowerCase();
+    if (query) {
+      query = query.trim().toLowerCase();
 
-    if (query.length > MIN_QUERY_LENGTH) {
-      return typeaheadSearch(endpoint, query);
+      if (query.length >= MIN_QUERY_LENGTH) {
+        return typeaheadSearch(endpoint, query);
+      }
     }
+
+    return clearSuggestions();
+  },
+
+  handleBlur ({
+    value,
+    suggestionFormatter,
+    updateSuggestionInputValue,
+    helper
+  }) {
+    const savedValue = helper.value && suggestionFormatter(helper.value.data);
+
+    // If the saved value in the redux-form matches the displayed value in the
+    // auto-suggest input, we know that the user hasn‘t changed his choice.
+    // If it did, we must reset both values as they aren‘t valid anymore.
+    if (savedValue !== value) {
+      updateSuggestionInputValue('');
+      helper.onChange('');
+    }
+  },
+
+  handleChange (newValue, { updateSuggestionInputValue, helper }) {
+    // We have to manually update the displayed value in the auto-suggest.
+    // This has nothing to do with the actual redux-form value.
+    updateSuggestionInputValue(newValue);
+
+    // If there is no more value, then we know the user has deleted his choice,
+    // and we can update the redux-form store.
+    if (!newValue) {
+      helper.onChange('');
+    }
+  },
+
+  handleSelection (e, { suggestion }, helper) {
+    // Disable form submitting when selecting option with ENTER
+    e.preventDefault();
+
+    // Update redux-form store manually to reflect the selection
+    helper.onChange({
+      data: suggestion,
+      id: suggestion.id,
+      class: 'City'
+    });
+  },
+
+  render () {
+    return (
+      <Autosuggest
+        theme={{
+          containerOpen: styles.open,
+          input: this.getClassname('input', fieldIsInvalid(this.props.helper)),
+          suggestionsList: styles.list,
+          suggestion: styles.suggestion,
+          suggestionFocused: styles.focused
+        }}
+        id={this.props.name}
+
+        suggestions={this.props.suggestions}
+        renderSuggestion={this.props.suggestionFormatter}
+        getSuggestionValue={this.props.suggestionFormatter}
+        focusFirstSuggestion
+
+        onSuggestionsFetchRequested={(e) => (
+          this.handleFetchRequest(e, {...this.props})
+        )}
+        onSuggestionsClearRequested={this.props.clearSuggestions}
+        onSuggestionSelected={(e, suggestion) => (
+          this.handleSelection(e, suggestion, this.props.helper)
+        )}
+
+        inputProps={{
+          name: this.props.name,
+          id: this.props.name,
+          required: this.props.html.required,
+          placeholder: this.props.html.placeholder,
+          autoComplete: this.props.html.autocomplete || 'off',
+
+          ...domOnlyProps(this.props.helper),
+
+          value: this.props.value,
+          onBlur: () => (
+            this.handleBlur({...this.props})
+          ),
+          onChange: (e, { newValue }) => (
+            this.handleChange(newValue, {...this.props})
+          )
+        }}
+      />
+    );
   }
-
-  return clearSuggestions();
-};
-
-const handleBlur = (displayedValue, displaySuggestion, updateSuggestionInputValue, helper) => {
-  const savedValue = helper.value && displaySuggestion(helper.value.data);
-
-  // If the saved value in the redux-form matches the displayed value in the
-  // auto-suggest input, we know that the user hasn‘t changed his choice.
-  // If it did, we must reset both values as they aren‘t valid anymore.
-  if (savedValue !== displayedValue) {
-    updateSuggestionInputValue('');
-    helper.onChange('');
-  }
-};
-
-const handleChange = (newValue, updateSuggestionInputValue, helper) => {
-  // We have to manually update the displayed value in the auto-suggest.
-  // This has nothing to do with the actual redux-form value.
-  updateSuggestionInputValue(newValue);
-
-  // If there is no more value, then we know the user has deleted his choice,
-  // and we can update the redux-form store.
-  if (!newValue) {
-    helper.onChange('');
-  }
-};
-
-const handleSelection = (e, { suggestion }, helper) => {
-  // Disable form submitting when selecting option with ENTER
-  e.preventDefault();
-
-  // Update redux-form store manually to reflect the selection
-  helper.onChange({
-    data: suggestion,
-    id: suggestion.id,
-    class: 'City'
-  });
-};
-
-const Autocomplete = ({
-  endpoint,
-  suggestionFormatter,
-  typeaheadSearch,
-  clearSuggestions,
-  updateSuggestionInputValue,
-  suggestions,
-  value,
-  helper,
-  name,
-  html
-}) => (
-  <Autosuggest
-    theme={{
-      containerOpen: styles.open,
-      input: getClassname('input', fieldIsInvalid(helper)),
-      suggestionsList: styles.list,
-      suggestion: styles.suggestion,
-      suggestionFocused: styles.focused
-    }}
-    id={name}
-
-    suggestions={suggestions}
-    renderSuggestion={suggestionFormatter}
-    getSuggestionValue={suggestionFormatter}
-    focusFirstSuggestion
-
-    onSuggestionsFetchRequested={(e) => (
-      handleFetchRequest(endpoint, e, typeaheadSearch, clearSuggestions)
-    )}
-    onSuggestionsClearRequested={clearSuggestions}
-    onSuggestionSelected={(e, option) => (
-      handleSelection(e, option, helper)
-    )}
-
-    inputProps={{
-      name: name,
-      id: name,
-      required: html.required,
-      placeholder: html.placeholder,
-      autoComplete: html.autocomplete || 'off',
-
-      ...domOnlyProps(helper),
-
-      value: value,
-      onBlur: () => (
-        handleBlur(value, suggestionFormatter, updateSuggestionInputValue, helper)
-      ),
-      onChange: (e, { newValue }) => (
-        handleChange(newValue, updateSuggestionInputValue, helper)
-      )
-    }}
-  />
-);
+});
 
 export default Autocomplete;
