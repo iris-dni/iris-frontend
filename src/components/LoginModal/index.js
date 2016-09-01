34090ac6a219a02b7ee7bb02768aea901111b576
Matt Berridge <hi@builtbyboon.com>
2016-09-01 13:46:42 +0200

    Renove unused modal styles

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import styles from './login-modal.scss';
 import settings from 'settings';
 import SsoProviders from 'components/SsoProviders';
 import ModalIntro from 'components/ModalIntro';
 
 const LoginModal = ({ ssoProviders }) => (
-  <div className={styles.root}>
+  <div>
     <ModalIntro title={'Nearly there'} intro={settings.loginPage.intro} />
     <SsoProviders providers={ssoProviders} />
   </div>
