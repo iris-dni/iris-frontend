a4581ebd7d8542a1b8fdd1389c974dbf0625f4a9
Matt Berridge <hi@builtbyboon.com>
2016-09-02 15:36:51 +0200

    Modal styling

--- src/components/ModalIntro/index.js
+++ src/components/ModalIntro/index.js
@@ -3,12 +3,14 @@ import styles from './modal-intro.scss';
 import TextCenter from 'components/TextCenter';
 
 const ModalIntro = ({ title, intro }) => (
-  <TextCenter>
-    {title && <h2 className={styles.title}>{title}</h2>}
-    <p className={styles.intro}>
-      {intro}
-    </p>
-  </TextCenter>
+  <div className={styles.root}>
+    <TextCenter>
+      {title && <h2 className={styles.title}>{title}</h2>}
+      <p className={styles.intro}>
+        {intro}
+      </p>
+    </TextCenter>
+  </div>
 );
 
 export default ModalIntro;
