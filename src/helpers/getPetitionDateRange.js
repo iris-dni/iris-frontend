dc01df7ae032151cefeb22f58828ec5c10d5581b
Matt Berridge <hi@builtbyboon.com>
2016-09-06 18:18:34 +0200

    Fix end date display on Petition page

--- src/helpers/getPetitionDateRange.js
+++ src/helpers/getPetitionDateRange.js
@@ -2,10 +2,10 @@ import moment from 'moment';
 import getPetitionEndDate from 'helpers/getPetitionEndDate';
 import settings from 'settings';
 
-export default ({ created, effective, expires }) => {
+export default ({ dc }) => {
+  const { created, effective, expires } = dc;
   const startDate = moment(effective || created);
   const endDate = getPetitionEndDate({ created, expires });
-
   return settings.dateRange
     .replace('%s', startDate.format(settings.dateFormat))
     .replace('%e', endDate.format(settings.dateFormat));
