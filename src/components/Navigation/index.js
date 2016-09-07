ea835d3435344906d8bf50b245dc3fec419ad3bb
Tom Bonnike <bonniketom@gmail.com>
2016-09-07 12:46:00 +0200

    Handled resize event to close mobile Navigation menu

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,6 +4,8 @@ import Container from 'components/Container';
 import Logo from 'components/Logo';
 import styles from './navigation.scss';
 
+const isBrowser = (typeof window !== 'undefined');
+
 const NAVIGATION_LINKS = [
   {
     label: 'Browse petitions',
@@ -31,6 +33,30 @@ const Navigation = React.createClass({
     };
   },
 
+  resizeHandler: function (e) {
+    var windowWidth = (isBrowser ? window.innerWidth : 0);
+
+    if (windowWidth >= 930) {
+      this.setState({
+        opened: false
+      });
+    }
+  },
+
+  componentWillMount: function () {
+    this.resizeHandler();
+  },
+  componentDidMount: function () {
+    if (isBrowser) {
+      window && window.addEventListener('resize', this.resizeHandler);
+    }
+  },
+  componentWillUnmount: function () {
+    if (isBrowser) {
+      window && window.removeEventListener('resize', this.resizeHandler);
+    }
+  },
+
   getClassName (defaultClass) {
     const opened = this.state.opened
       ? styles.opened
@@ -54,40 +80,38 @@ const Navigation = React.createClass({
         <div className={this.getClassName(styles.overlay)}></div>
 
         <Container>
-          <div className={styles['menu-wrapper']}>
-            <div className={styles['logo-wrapper']}>
-              <IndexLink to='home' className={styles.link}>
-                <Logo />
-              </IndexLink>
+          <div className={styles['logo-wrapper']}>
+            <IndexLink to='home' className={styles.link}>
+              <Logo />
+            </IndexLink>
 
-              <div
-                className={this.getClassName(styles['burger-wrapper'])}
-                onClick={this.openMenu}
-              >
-                <span>Menu</span>
+            <div
+              className={this.getClassName(styles['burger-wrapper'])}
+              onClick={this.openMenu}
+            >
+              <span>Menu</span>
 
-                <div className={styles['burger-menu']}>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
+              <div className={styles['burger-menu']}>
+                <span></span>
+                <span></span>
+                <span></span>
               </div>
             </div>
-
-            <ul className={this.getClassName(styles.list)}>
-              {NAVIGATION_LINKS.map((link, key) => (
-                <li className={styles.element} key={key}>
-                  <Link
-                    to={link.path}
-                    className={styles.link}
-                    activeClassName={styles.active}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
           </div>
+
+          <ul className={this.getClassName(styles.list)}>
+            {NAVIGATION_LINKS.map((link, key) => (
+              <li className={styles.element} key={key}>
+                <Link
+                  to={link.path}
+                  className={styles.link}
+                  activeClassName={styles.active}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </Container>
       </nav>
     );
