4786b91cbe7fe058c3c329ff38d19ca73e0a0ab5
Matt Berridge <hi@builtbyboon.com>
2016-09-09 14:45:34 +0200

    Fix logic for accessing edit form direclty

--- src/views/Home.js
+++ src/views/Home.js
@@ -7,10 +7,7 @@ export default (props) => (
     <h1>Home Component</h1>
     <p><LoginLink>Login</LoginLink></p>
     <p><Link to='/petitions/new'>Create new Petition (client)</Link></p>
-    <p><a href='/petitions/new'>Create new Petition (server)</a></p>
     <p><Link to='/petitions/1ESkp'>Go to petition 1ESkp</Link></p>
-    <p><Link to='/petitions/1ESkp/edit'>Edit petition 1ESkp</Link></p>
-    <p><Link to='/petitions/1jWQQ/edit'>Edit petition 1jWQQ</Link></p>
     <p><Link to='/petitions'>Go to petitions</Link></p>
     <p>
       Go to petitions on Page
