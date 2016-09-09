0d5a4c6f7a3ca9c302b8c39101f3e9cf3ec02567
Matt Berridge <hi@builtbyboon.com>
2016-09-09 17:48:40 +0200

    Adds pagination props generator and prints more metrics to page

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -3,12 +3,14 @@ import React from 'react';
 const Pagination = ({
   total,
   showing,
+  limit,
   currentPage,
   pages
 }) => (
   <nav>
     Total: {total}
     Showing: {showing}
+    Limit: {limit}
     Page: {currentPage}
     Pages: {pages}
   </nav>
--- src/helpers/getPaginationFromParams.js
+++ src/helpers/getPaginationFromParams.js
@@ -2,6 +2,7 @@ export default (showing, total, params = {}) => {
   const { page, limit } = params;
   return {
     showing: showing,
+    limit: limit,
     total: total,
     pages: page,
     currentPage: Math.ceil(showing / limit),
