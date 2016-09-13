4b46a615538e608c3b6649faa43dffd083c611cc
Eric Schäfer <omg@eric-schaefer.com>
2016-09-13 17:20:25 +0200

    change to assert style

--- test/components/Pagination.js
+++ test/components/Pagination.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
+import { assert } from 'chai';
 import Pagination from 'components/Pagination';
 import styles from 'components/Pagination/pagination.scss';
 
 describe('<Pagination />', () => {
   it('does not render without > 1 pages', () => {
     const wrapper = shallow(<Pagination totalPages={1} />);
-    expect(wrapper.hasClass(`${styles.root}`)).to.equal(false);
+    const actual = wrapper.hasClass(`${styles.root}`);
+    const expected = false;
+
+    assert.equal(actual, expected);
   });
 
   it('does render when > 1 pages', () => {
     const wrapper = shallow(<Pagination totalPages={4} />);
-    expect(wrapper.find(`.${styles.root}`)).to.not.be.empty;
+    const actual = wrapper.find(`.${styles.root}`).length;
+    const expected = 1;
+
+    assert.equal(actual, expected);
   });
 
   it('does not render "previous" link when on first page', () => {
     const wrapper = shallow(<Pagination totalPages={4} isFirstPage isLastPage={false} />);
-    expect(wrapper.find(Link)).to.have.length(1);
+    const actual = wrapper.find(Link).length;
+    const expected = 1;
+
+    assert.equal(actual, expected);
   });
 
   it('does not render "next" link when on last page', () => {
     const wrapper = shallow(<Pagination totalPages={4} isFirstPage={false} isLastPage />);
-    expect(wrapper.find(Link)).to.have.length(1);
-    expect(wrapper.find(Link).props().rel).to.equal('prev');
+    const actual = wrapper.find(Link).props().rel;
+    const expected = 'prev';
+
+    assert.equal(actual, expected);
   });
 
   it('does not render "prev" link when on first page', () => {
     const wrapper = shallow(<Pagination totalPages={4} isFirstPage isLastPage={false} />);
-    expect(wrapper.find(Link)).to.have.length(1);
-    expect(wrapper.find(Link).props().rel).to.equal('next');
+    const actual = wrapper.find(Link).props().rel;
+    const expected = 'next';
+
+    assert.equal(actual, expected);
   });
 
   it('renders both "previous" and "next" links if not first or last page', () => {
     const wrapper = shallow(<Pagination totalPages={4} isLastPage={false} isFirstPage={false} />);
-    expect(wrapper.find(Link)).to.have.length(2);
+    const actual = wrapper.find(Link).length;
+    const expected = 2;
+
+    assert.equal(actual, expected);
   });
 });
