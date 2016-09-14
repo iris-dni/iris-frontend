2ab33e7bf2217d65bb7d48e6ebb3e620b86a564c
Eric Schäfer <omg@eric-schaefer.com>
2016-09-14 11:52:27 +0200

    Fix routes to support active links a little better.

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Router, IndexRedirect, browserHistory } from 'react-router';
+import { Route, Router, IndexRedirect, browserHistory, IndexRoute } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import App from 'containers/App';
@@ -26,11 +26,15 @@ export default function (props = {}) {
         <Route path='home' component={Home} />
         <Route path='auth/login' component={LoginPage} />
         <Route path='auth/logout' component={Logout} />
-        <Route path='petitions(/:cityName-:city)(/page/:page)' component={Petitions} />
-        <Route path='petitions/new' component={NewPetition} />
-        <Route path='petitions/:id/edit' component={EditPetition} />
-        <Route path='petitions/:id/published' component={PublishedPetition} />
-        <Route path='petitions/:id' component={Petition} />
+        <Route path='petitions'>
+          <IndexRoute component={Petitions} />
+          <Route path=':cityName-:city(/page/:page)' component={Petitions} />
+          <Route path='page/:page' component={Petitions} />
+        </Route>
+        <Route path='/petitions/new' component={NewPetition} />
+        <Route path='/petitions/:id' component={Petition} />
+        <Route path='/petitions/:id/edit' component={EditPetition} />
+        <Route path='/petitions/:id/published' component={PublishedPetition} />
       </Route>
     </Router>
   );
