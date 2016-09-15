767a8fdbd52ab046ee4047f6e0baf453364c99b9
Eric Schäfer <omg@eric-schaefer.com>
2016-09-15 12:34:53 +0200

    Add loading styles using the existing CircleProgressBar

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import CircleProgressBar from 'components/CircleProgressBar';
+import styles from './loading.scss';
 
-const Loading = ({ isLoading, children, onServer }) => (
-  onServer || (__CLIENT__ && isLoading) || !children
-    ? <div>Loading screen</div>
-    : children
-);
+const Loading = React.createClass({
+
+  getInitialState () {
+    return { percentage: 75 };
+  },
+
+  tick () {
+    this.setState({ percentage: this.state.percentage + 125 });
+  },
+
+  componentDidMount () {
+    this.interval = setInterval(this.tick, 1000);
+  },
+
+  componentWillUnmount () {
+    clearInterval(this.interval);
+  },
+
+  render () {
+    const { isLoading, children, onServer } = this.props;
+
+    if (onServer || (__CLIENT__ && isLoading) || !children) {
+      return (
+        <div
+          className={styles.root}
+          aria-busy='true'>
+          <CircleProgressBar
+            percentage={this.state.percentage}
+            animated
+          />
+        </div>
+      );
+    }
+
+    return children;
+  }
+});
 
 export default Loading;
