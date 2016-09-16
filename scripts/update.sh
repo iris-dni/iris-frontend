efbe44a0902a61e4f249e67daf371a94e85cb35c
Eric Schäfer <omg@eric-schaefer.com>
2016-09-16 16:11:05 +0200

    add update script

---
+++ scripts/update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ORIGINAL_REPO=iris-dni/iris-frontend.git
+HAS_CONFIGURED_UPSTREAM=$(git config --get remote.upstream.url | grep $ORIGINAL_REPO)
+
+if [ "$HAS_CONFIGURED_UPSTREAM" ]; then
+  read -p "You should definitely stash any changes before doing this. Continue? [Y/n] " -n 1 -r
+  echo # blank line
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Fetching updates from upstream..."
+    git fetch upstream
+    git checkout master
+    git rebase upstream/master
+  fi
+else
+  echo "Nothing to do here. Probably the original repo."
+fi
