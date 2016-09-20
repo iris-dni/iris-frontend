7f753aff3b310afb2b5e5b9d4f64049ca09a12f6
Matt Berridge <hi@builtbyboon.com>
2016-09-20 14:27:42 +0200

    Remove console log

--- src/helpers/getPetitionEndDate.js
+++ src/helpers/getPetitionEndDate.js
@@ -2,7 +2,6 @@ import moment from 'moment';
 import settings from 'settings';
 
 export default ({ created, expires }) => {
-  console.log('expires:', expires);
   return expires
     ? moment(expires)
     : moment(created).add(settings.daysToVote, 'days');
