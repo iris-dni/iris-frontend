4790ccfa8d87f3dfa7a7dc3f83a24ee836cc2761
Matt Berridge <hi@builtbyboon.com>
2016-09-21 11:05:12 +0200

    Abstract params properties

--- src/helpers/getPetitionsRequestParams.js
+++ src/helpers/getPetitionsRequestParams.js
@@ -1,21 +1,17 @@
 import calculateParamOffset from 'helpers/calculateParamOffset';
 import sanitizeParamLimit from 'helpers/sanitizeParamLimit';
 import getPetitionsStates from 'helpers/getPetitionsStates';
+import getPetitionsSortParam from 'helpers/getPetitionsSortParam';
 
 export default ({ limit, page, city, sort, state }) => {
   const saneLimit = sanitizeParamLimit(limit);
 
-  sort = (sort === 'supporters') ? '-supporters.amount' : '-created';
-  state = getPetitionsStates(state);
-
-  const params = {
+  return {
     resolve: 'city,owner',
     offset: calculateParamOffset(page, saneLimit),
     limit: saneLimit,
     city: city || '',
-    state,
-    sort
+    state: getPetitionsStates(state),
+    sort: getPetitionsSortParam(sort)
   };
-
-  return params;
 };
