81cb07dc7abebf506147ce94a00a27477cf2c58a
Matt Berridge <hi@builtbyboon.com>
2016-09-22 16:10:05 +0200

    Fill in base OG data from petition

--- src/selectors/petitionSchema.js
+++ src/selectors/petitionSchema.js
@@ -1,16 +1,18 @@
+import getPetitionStartDate from 'helpers/getPetitionStartDate';
+
 export default (petition = {}) => ({
   '@context': 'http://schema.org',
   '@type': 'Question',
   'name': petition.title,
   'upvoteCount': petition.supporters.amount,
   'text': petition.description,
-  'dateCreated': petition.dc.effective || petition.dc.created,
+  'dateCreated': getPetitionStartDate(petition.dc || {}),
   'author': { /* todo */ },
   'suggestedAnswer': {
     '@type': 'Answer',
     'upvoteCount': petition.supporters.amount,
     'text': petition.suggested_solution,
-    'dateCreated': petition.dc.effective || petition.dc.created,
+    'dateCreated': getPetitionStartDate(petition.dc || {}),
     'author': { /* todo */ }
   }
 });
