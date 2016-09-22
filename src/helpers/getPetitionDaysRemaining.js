3cbedcf697029d844f472c6d2503f47edee6506d
Matt Berridge <hi@builtbyboon.com>
2016-09-22 15:04:04 +0200

    Remove unused param from daysRemaining function

--- src/helpers/getPetitionDaysRemaining.js
+++ src/helpers/getPetitionDaysRemaining.js
@@ -1,9 +1,9 @@
 import moment from 'moment';
 import getPetitionEndDate from 'selectors/petitionEndDate';
 
-export default ({ created, expires }, timeNow) => {
+export default ({ created, expires }) => {
   const endDate = getPetitionEndDate({ created, expires });
-  const nowDate = timeNow || moment().valueOf();
-  const daysRemaining = moment.duration(moment(endDate).diff(nowDate)).asDays();
+  const nowDate = moment().valueOf();
+  const daysRemaining = moment.duration(endDate.diff(nowDate)).asDays();
   return Math.max(Math.round(daysRemaining), 0);
 };
