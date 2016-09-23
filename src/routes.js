24aae7f9e4ae81fc6d264049332fad50a7ad3a91
Eric Schäfer <omg@eric-schaefer.com>
2016-09-23 14:44:16 +0200

    change route for response page

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Petitions from 'containers/Petitions';
 import NewPetition from 'containers/NewPetition';
 import EditPetition from 'containers/EditPetition';
 import PublishedPetition from 'containers/PublishedPetition';
-import AnswerPetition from 'containers/AnswerPetition';
+import RespondToPetition from 'containers/RespondToPetition';
 
 export default function (props = {}) {
   let history = browserHistory;
@@ -37,7 +37,7 @@ export default function (props = {}) {
         <Route path='petitions/:id' component={Petition} />
         <Route path='petitions/:id/edit' component={EditPetition} />
         <Route path='petitions/:id/published' component={PublishedPetition} />
-        <Route path='petitions/answer/:token' component={AnswerPetition} />
+        <Route path='respond/:token' component={RespondToPetition} />
       </Route>
     </Router>
   );
