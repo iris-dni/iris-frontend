75350cc5abeae53f12fe00eedc7fdce7271c7cfe
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2016-09-23 12:02:28 +0200

    Add respondToPetition action using respond

--- src/actions/RespondActions.js
+++ src/actions/RespondActions.js
@@ -1,9 +1,17 @@
 import petitionRepository from 'services/api/repositories/petition';
+import solveResolvedObjects from 'helpers/solveResolvedObjects';
+import settings from 'settings';
 
 import {
   requestPetition,
-  receivePetition
+  receivePetition,
+  submittingPetition,
+  updatedPetition
 } from 'actions/PetitionActions';
+
+import {
+  showFlashMessage
+} from './FlashActions';
 
 export function fetchPetitionByResponseToken (responseToken) {
   return (dispatch, getState) => {
@@ -13,4 +21,17 @@ export function fetchPetitionByResponseToken (responseToken) {
         receivePetition(response.data)
       ));
   };
+}
+
+export function respondToPetition (petition, response, dispatch) {
+  dispatch(submittingPetition());
+  return petitionRepository.respond(petition, response)
+    .then((response) => {
+      const resolvedPetition = solveResolvedObjects(petition, response.data);
+      dispatch(updatedPetition(resolvedPetition));
+    }).then(() => dispatch(
+      showFlashMessage(settings.flashMessages.petitionUpdated, 'success')
+    )).catch(() => dispatch(
+      showFlashMessage(settings.flashMessages.genericError, 'error')
+    ));
 }
