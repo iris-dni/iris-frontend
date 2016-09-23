ecd531de411c20dc78f8e5c8bd5a39723b04588f
Matt Berridge <hi@builtbyboon.com>
2016-09-23 16:47:33 +0200

    Aria

--- src/components/SocialLink/index.js
+++ src/components/SocialLink/index.js
@@ -4,7 +4,7 @@ import Icon from 'components/Icon';
 
 const SocialLink = ({ href, modifier, icon, label }) => (
   <a href={href} title={label} className={styles[modifier]}>
-    <span className={styles.icon}>
+    <span className={styles.icon} aria-hidden aria-role={'presentation'}>
       <Icon id={icon} />
     </span>
     <span className={styles.label}>{label}</span>
