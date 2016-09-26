6b2bf550d6dec92657195f469c968e3a0a9cae1e
Eric Schäfer <omg@eric-schaefer.com>
2016-09-26 17:04:25 +0200

    add optional no margin to paragraph component

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styles from './paragraph.scss';
 
-const Paragraph = ({ children, text }) => (
-  <p className={styles.root}>
+const Paragraph = ({ children, text, noMargin }) => (
+  <p className={styles[noMargin ? 'no-margin' : 'root']}>
     {children || text}
   </p>
 );
