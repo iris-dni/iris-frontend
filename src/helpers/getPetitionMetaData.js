174c708e9102b9632a1ea429e8d5d75950fd83ac
Matt Berridge <hi@builtbyboon.com>
2016-09-26 17:29:04 +0200

    Adds twitter cards while we are here

--- src/helpers/getPetitionMetaData.js
+++ src/helpers/getPetitionMetaData.js
@@ -1,7 +1,9 @@
 import getPetitionMetaData from 'selectors/petitionMetaData';
 import getPetitionOpenGraph from 'selectors/petitionOpenGraph';
+import getPetitionTwitterCard from 'selectors/petitionTwitterCard';
 
 export default (petition = {}) => ([].concat(
   getPetitionMetaData(petition),
-  getPetitionOpenGraph(petition)
+  getPetitionOpenGraph(petition),
+  getPetitionTwitterCard(petition)
 ));
