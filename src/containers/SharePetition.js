8b50f19394e04221ff838c3d17d772ee9bafee20
Matt Berridge <hi@builtbyboon.com>
2016-09-27 17:54:06 +0200

    Add grey background to petition header

--- src/containers/SharePetition.js
+++ src/containers/SharePetition.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ShareButtons from 'components/ShareButtons';
-import openShareWindow from 'helpers/openShareWindow';
-import generateShareLinks from 'helpers/generateShareLinks';
+import openShareWindow from 'helpers/sharing/openShareWindow';
+import generateShareButtons from 'helpers/sharing/generateShareButtons';
 
-const POPUP_WINDOW_TITLE = 'Share this page';
+const POPUP_WINDOW_TITLE = 'Share this petition';
 
 const SharePetitionContainer = (props) => (
   <ShareButtons {...props} />
@@ -14,7 +14,7 @@ const openPopup = (url, event) => openShareWindow(url, event, POPUP_WINDOW_TITLE
 
 const mapStateToProps = ({ petition }) => ({
   openPopup: openPopup,
-  buttons: generateShareLinks(petition.id)
+  buttons: generateShareButtons(petition.id)
 });
 
 SharePetitionContainer.propTypes = {
