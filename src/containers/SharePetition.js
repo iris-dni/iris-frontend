4eef6c78a5516eabd86dc5f00ba97fc9a1fbf1a0
Matt Berridge <hi@builtbyboon.com>
2016-09-27 17:12:43 +0200

    Adds translation strings and sharing copy

--- src/containers/SharePetition.js
+++ src/containers/SharePetition.js
@@ -2,15 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ShareButtons from 'components/ShareButtons';
 import openShareWindow from 'helpers/openShareWindow';
+import generateShareLinks from 'helpers/generateShareLinks';
 
-import {
-  facebookShareButton,
-  twitterShareButton,
-  whatsappShareButton,
-  emailShareButton
-} from 'helpers/generateShareLinks';
-
-const POPUP_WINDOW_TITLE = 'Share Petition';
+const POPUP_WINDOW_TITLE = 'Share this page';
 
 const SharePetitionContainer = (props) => (
   <ShareButtons {...props} />
@@ -20,12 +14,7 @@ const openPopup = (url, event) => openShareWindow(url, event, POPUP_WINDOW_TITLE
 
 const mapStateToProps = ({ petition }) => ({
   openPopup: openPopup,
-  buttons: [
-    facebookShareButton(petition.id),
-    twitterShareButton(petition.id),
-    whatsappShareButton(petition.id),
-    emailShareButton(petition.id)
-  ]
+  buttons: generateShareLinks(petition.id)
 });
 
 SharePetitionContainer.propTypes = {
