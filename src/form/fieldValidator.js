d56b25afe7059bd6dda55a0c1f4ca66bcc686af6
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2016-09-28 14:17:30 +0200

    Fix validation bug

--- src/form/fieldValidator.js
+++ src/form/fieldValidator.js
@@ -1,5 +1,5 @@
 import settings from 'settings';
-import { get } from 'lodash/object';
+import { get, set } from 'lodash/object';
 
 export default (fields, values) => {
   const errors = {};
@@ -10,20 +10,20 @@ export default (fields, values) => {
     }
 
     const key = field.name;
-    const value = get(values, field.name);
+    const value = get(values, key);
     const isRequired = field.html && field.html.required;
     const minLength = field.html && field.html.minLength;
     const maxLength = field.html && field.html.maxLength;
 
     if (isRequired && values && !value) {
       // test for required fields
-      errors[key] = settings.requiredText;
+      set(errors, key, settings.requiredText);
     } else if (maxLength && value && value.length > maxLength) {
       // test against maxLength
-      errors[key] = settings.maxLengthText.replace('%x', maxLength);
+      set(errors, key, settings.maxLengthText.replace('%x', maxLength));
     } else if (minLength && value && value.length < minLength) {
       // test against minLength
-      errors[key] = settings.minLengthText.replace('%x', minLength);
+      set(errors, key, settings.minLengthText.replace('%x', minLength));
     }
   });
 
