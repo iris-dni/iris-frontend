555d2579e454c974b18d48bbcfd83224f9cabf53
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2016-09-29 21:32:05 +0200

    Style PetitionReponseStatus component

--- src/components/PetitionResponseStatus/index.js
+++ src/components/PetitionResponseStatus/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import styles from './petition-response-status.scss';
 import getPetitionResponseStatusTitle from 'helpers/getPetitionResponseStatusTitle';
 import getPetitionResponseStatusText from 'helpers/getPetitionResponseStatusText';
+import Icon from 'components/Icon';
 
 const PetitionResponseStatus = (props) => (
   <div className={styles.root} >
-    <h2>{getPetitionResponseStatusTitle(props)}</h2>
+    <div className={styles.top}>
+      <span className={styles.icon}>
+        <Icon id='Flag' size='small' modifier='highlight' />
+      </span>
+      <h2>
+        {getPetitionResponseStatusTitle(props)}
+      </h2>
+    </div>
     <p>{getPetitionResponseStatusText(props)}</p>
   </div>
 );
