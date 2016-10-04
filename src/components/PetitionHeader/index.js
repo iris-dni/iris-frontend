e975c87123caf80ca2c680da15d2231561298f19
Eric Schäfer <omg@eric-schaefer.com>
2016-10-04 10:37:56 +0200

    Integrate PetitionTags container

--- src/components/PetitionHeader/index.js
+++ src/components/PetitionHeader/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './petition-header.scss';
 import Header from 'components/Header';
 import PageTitle from 'components/PageTitle';
+import PetitionTags from 'containers/PetitionTags';
 import PetitionInfo from 'containers/PetitionInfo';
 import PetitionProgress from 'containers/PetitionProgress';
 import PetitionStats from 'containers/PetitionStats';
 
 const PetitionHeader = ({ title }) => (
   <Header>
+    <PetitionTags />
     <PageTitle title={title} />
     <div className={styles.info}>
       <PetitionInfo />
