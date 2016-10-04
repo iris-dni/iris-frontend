a029bda8409f80ae71b5b8fe68a66ef947330efb
Eric Schäfer <omg@eric-schaefer.com>
2016-10-04 10:37:12 +0200

    Add new PetitionTags component and container

---
+++ src/components/PetitionTags/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './petition-tags.scss';
+import Icon from 'components/Icon';
+
+const PetitionTags = ({ winner, response }) => {
+  if (!winner && !response.text) {
+    return null;
+  }
+
+  return (
+    <div className={styles.root}>
+      {winner &&
+        <div className={styles.winner}>
+          <span className={styles.icon}>
+            <Icon id='Flag' size='tiny' modifier='invert' />
+          </span>
+          <span>Winner</span>
+        </div>}
+
+      {response.text &&
+        <div className={styles.response}>
+          <span className={styles.icon}>
+            <Icon id='Note' size='tiny' modifier='invert' />
+          </span>
+          <span>Response</span>
+        </div>}
+    </div>
+  );
+};
+
+export default PetitionTags;
