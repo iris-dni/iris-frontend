304d2b66de78883797bc7e2b84a5673d296e18d9
Matt Berridge <hi@builtbyboon.com>
2016-10-04 16:50:42 +0200

    Rebase fix

--- src/helpers/getPetitionDaysRemaining.js
+++ src/helpers/getPetitionDaysRemaining.js
@@ -4,6 +4,6 @@ import getPetitionEndDate from 'selectors/petitionEndDate';
 export default ({ created, expires }) => {
   const endDate = getPetitionEndDate({ created, expires });
   const nowDate = moment().valueOf();
-  const daysRemaining = moment.duration(endDate.diff(nowDate)).asDays();
+  const daysRemaining = moment.duration(moment(endDate).diff(nowDate)).asDays();
   return Math.max(Math.round(daysRemaining), 0);
 };
