7b3f24bc6a9d97f608cfe73d7dfe38e94e2a1307
Tom Bonnike <bonniketom@gmail.com>
2016-10-04 18:42:52 +0200

    Fixed similar URLs with or without protocol

--- src/form/getLinkInputErrors.js
+++ src/form/getLinkInputErrors.js
@@ -5,12 +5,17 @@ export default function (currentValue, links) {
   let error = '';
   const LINKS_SETTINGS = settings.petitionFields.links;
   const MAX_LINKS = LINKS_SETTINGS.maxLinks;
+  const PROTOCOL_REGEX = new RegExp(/^(https?:\/\/)/i);
+
+  const similarLinks = links.filter(link => (
+    currentValue.replace(PROTOCOL_REGEX, '') === link.url.replace(PROTOCOL_REGEX, '')
+  ));
 
   if (!isLink(currentValue)) {
     error = LINKS_SETTINGS.invalidLinkFormat;
   } else if (links.length >= MAX_LINKS) {
     error = LINKS_SETTINGS.invalidLinkCount.replace('%x', MAX_LINKS);
-  } else if (links.filter(link => (currentValue === link.url)).length) {
+  } else if (similarLinks.length) {
     error = LINKS_SETTINGS.invalidSimilarLink;
   }
 
