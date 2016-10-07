a30578cd6a35ca1170ad7b9c5d3b4f936ad4d1d2
Eric Schäfer <omg@eric-schaefer.com>
2016-10-07 18:14:11 +0200

    Organise HomepagePetitions

--- src/components/HomepagePetitions/index.js
+++ src/components/HomepagePetitions/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import TeaserGrid from 'components/TeaserGrid';
+import Container from 'components/Container';
+import Section from 'components/Section';
+import Heading2 from 'components/Heading2';
+import TextCenter from 'components/TextCenter';
 
 const HomepagePetitions = ({ petitions, title }) => (
   <section>
-    <TeaserGrid petitions={petitions} />
+    <Section>
+      <Container>
+        <TextCenter>
+          <Heading2 text={title} />
+        </TextCenter>
+        <TeaserGrid petitions={petitions} />
+      </Container>
+    </Section>
   </section>
 );
 
