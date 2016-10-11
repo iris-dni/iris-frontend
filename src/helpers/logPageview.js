18d1a64f0f5f73929e4d4adaaf690bdf6994fedc
Eric Schäfer <omg@eric-schaefer.com>
2016-10-11 18:23:54 +0200

    Add new SITE_NAME env var which will be needed for namespacing page view events

--- src/helpers/logPageview.js
+++ src/helpers/logPageview.js
@@ -6,9 +6,10 @@ export default () => {
   if (__CLIENT__) {
     const { pathname } = window.location;
     const { ga } = settings;
+    const pageviewEventName = `${process.env.SITE_NAME}_pageview`.trim();
 
     const pageviewEvent = typeof CustomEvent !== 'undefined' && // Required as it’s not defined during tests.
-      new CustomEvent('IRIS_pageview');
+      new CustomEvent(pageviewEventName);
 
     if (ga.APIKey) {
       ReactGA.set({ page: pathname });
