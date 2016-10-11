5e10101636dfe39377661a0e75b79cc712fbc24a
Matt Berridge <hi@builtbyboon.com>
2016-10-11 14:08:47 +0200

    Adds submitted data to me object

--- src/actions/SupportActions.js
+++ src/actions/SupportActions.js
@@ -1,6 +1,7 @@
 import petitionRepository from 'services/api/repositories/petition';
 import getPetitionURL from 'helpers/getPetitionURL';
 import isUntrustedUser from 'helpers/isUntrustedUser';
+import { receiveWhoAmI } from 'actions/AuthActions';
 import settings from 'settings';
 
 import {
@@ -24,18 +25,20 @@ export function submittingSupport () {
 
 export function supportPetition (trustData, dispatch) {
   dispatch(submittingSupport());
-  const mockPetitionForNow = { id: trustData.petitionId };
+  // Add submitted data to me object for future
+  dispatch(receiveWhoAmI(trustData.user));
+  // Trigger support action
   return petitionRepository.support(trustData)
     .then((response) => {
       if (isUntrustedUser(response)) {
         // THIS WILL BE WHERE WE SHOW THE TAN PAGE
       }
       // THIS IS THE TRUSTED USER FLOW
-      return dispatch(supportedPetition(mockPetitionForNow));
+      return dispatch(supportedPetition(response.data));
     }).then((response) => dispatch(
       showModalWindow({
         type: 'supported',
-        link: getPetitionURL(mockPetitionForNow.id),
+        link: getPetitionURL(trustData.petitionId),
         ...settings.supportPetition.newlySupported.modal
       })
     )).catch((e) => {
