691859b42f83e3ddd3531631ec811a3b27a2190a
Matt Berridge <hi@builtbyboon.com>
2016-10-12 10:15:13 +0200

    Begin to add me data to form submissions

--- src/components/TrustConfirmationForm/index.js
+++ src/components/TrustConfirmationForm/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { reduxForm } from 'redux-form';
 import { supportPetition } from 'actions/SupportActions';
-import trustValidator from 'form/trustValidator';
+import trustConfirmationValidator from 'form/trustConfirmationValidator';
 import Fieldset from 'components/Fieldset';
 import FormFieldsIterator from 'components/FormFieldsIterator';
 import Button from 'components/Button';
 import FIELDS from './fields';
 
-const TrustConfirmationForm = ({ fields, handleSubmit, submitting }) => (
-  <form onSubmit={handleSubmit(supportPetition)}>
+const supportWithUserData = (data, dispatch) => {
+  const userData = Object.assign({}, data);
+  return supportPetition(userData, dispatch);
+};
+
+const TrustConfirmationForm = ({ fields, handleSubmit, submitting, me }) => (
+  <form onSubmit={handleSubmit(supportWithUserData)}>
     <Fieldset>
       <FormFieldsIterator
         reduxFormFields={fields}
@@ -17,7 +22,7 @@ const TrustConfirmationForm = ({ fields, handleSubmit, submitting }) => (
     </Fieldset>
     <Fieldset modifier={'actions'}>
       <Button
-        text={'Go to verification'}
+        text={'Complete verification'}
         modifier={'accent'}
         disabled={submitting || !fields._meta.allValid}
       />
@@ -35,11 +40,12 @@ export const mapStateToProps = ({ petition, me, trust }) => ({
   initialValues: {
     petitionId: petition.id
   },
+  me,
   submitting: trust.isSubmitting
 });
 
 export default reduxForm({
   form: 'trustConfirmation',
   fields: FIELDS.map(field => field.name),
-  validate: trustValidator
+  validate: trustConfirmationValidator
 }, mapStateToProps)(TrustConfirmationForm);
