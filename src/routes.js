c80f1c0006ccb6dd1bb4cfa6205d02b14782619f
Matt Berridge <hi@builtbyboon.com>
2016-10-14 11:32:29 +0200

    Rebase

--- src/routes.js
+++ src/routes.js
@@ -16,35 +16,25 @@ import RespondToPetition from 'containers/RespondToPetition';
 import logPageview from 'helpers/logPageview';
 import getHistory from 'helpers/getHistory';
 
-export default function (props = {}) {
-  const history = getHistory(props.store);
-
-  const { googleAnalytics } = settings;
-
-  if (googleAnalytics.APIKey.length) {
-    ReactGA.initialize(googleAnalytics.APIKey, googleAnalytics.initOptions);
-  }
-
-  return (
-    <Router history={history} onUpdate={logPageview}>
-      <Route path='/' component={App}>
-        <IndexRoute component={Home} />
-        <Route path='auth/login' component={LoginPage} />
-        <Route path='auth/logout' component={Logout} />
-        <Route path='trust/support/:id' component={TrustPage} />
-        <Route path='trust/support/:id/confirm' component={TrustConfirmationPage} />
-        <Route path='petitions'>
-          {/* Nest these 3 to support proper `activeClassName` behavior. */}
-          <IndexRoute component={Petitions} />
-          <Route path=':cityName-:city(/page(/:page))' component={Petitions} />
-          <Route path='page/:page' component={Petitions} />
-        </Route>
-        <Route path='petitions/new' component={NewPetition} />
-        <Route path='petitions/:id' component={Petition} />
-        <Route path='petitions/:id/edit' component={EditPetition} />
-        <Route path='petitions/:id/published' component={PublishedPetition} />
-        <Route path='respond/:token' component={RespondToPetition} />
+export default (props = {}) => (
+  <Router history={getHistory(props.store)} onUpdate={logPageview}>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home} />
+      <Route path='auth/login' component={LoginPage} />
+      <Route path='auth/logout' component={Logout} />
+      <Route path='trust/support/:id' component={TrustPage} />
+      <Route path='trust/support/:id/confirm' component={TrustConfirmationPage} />
+      <Route path='petitions'>
+        {/* Nest these 3 to support proper `activeClassName` behavior. */}
+        <IndexRoute component={Petitions} />
+        <Route path=':cityName-:city(/page(/:page))' component={Petitions} />
+        <Route path='page/:page' component={Petitions} />
       </Route>
-    </Router>
-  );
-}
+      <Route path='petitions/new' component={NewPetition} />
+      <Route path='petitions/:id' component={Petition} />
+      <Route path='petitions/:id/edit' component={EditPetition} />
+      <Route path='petitions/:id/published' component={PublishedPetition} />
+      <Route path='respond/:token' component={RespondToPetition} />
+    </Route>
+  </Router>
+);
