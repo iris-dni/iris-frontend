ab3cd9ca6832d58f9e2f99dddbf1b7427ad7494e
Eric Schäfer <omg@eric-schaefer.com>
2016-10-14 11:12:15 +0200

    Add translations, use BlockContainer

--- src/components/HomepagePetitions/index.js
+++ src/components/HomepagePetitions/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import TeaserGrid from 'components/TeaserGrid';
 import Container from 'components/Container';
+import BlockContainer from 'components/BlockContainer';
 import Section from 'components/Section';
 import Heading2 from 'components/Heading2';
-import TextCenter from 'components/TextCenter';
+import styles from './homepage-petitions.scss';
+import Link from 'components/Link';
 
-const HomepagePetitions = ({ petitions, title }) => (
+const HomepagePetitions = ({ petitions, title, text, linkText }) => (
   <section>
     <Section>
       <Container>
-        <TextCenter>
-          <Heading2 text={title} />
-        </TextCenter>
+        <BlockContainer>
+          <div className={styles.head}>
+            <Heading2 text={title} />
+            <Link href='/petitions'>{linkText}</Link>
+          </div>
+        </BlockContainer>
         <TeaserGrid petitions={petitions.latest} />
       </Container>
     </Section>
