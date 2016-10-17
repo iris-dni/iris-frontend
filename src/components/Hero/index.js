2c76fe14951563fdf85a804c9430bfc5e3879603
Eric Schäfer <omg@eric-schaefer.com>
2016-10-17 13:47:04 +0200

    Update Hero, might as well.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Heading1 from 'components/Heading1';
+import Container from 'components/Container';
 
 import styles from './hero.scss';
 
 const Hero = ({ title, missionTitle, missionDescription, background }) => (
   <section>
     <div className={styles.top}>
-      <div
-        style={{ backgroundImage: `url(${background})` }}
-        className={styles.background} />
-      <Heading1 text={title} />
+      <Container>
+        <div className={styles.inner}>
+          <div
+            style={{ backgroundImage: `url(${background})` }}
+            className={styles.background} />
+          <Heading1 text={title} />
+        </div>
+      </Container>
     </div>
   </section>
 );
