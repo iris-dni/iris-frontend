0dd68aeedd36240d9be1526454b3d5a95963c7e3
Eric Schäfer <omg@eric-schaefer.com>
2016-10-17 13:17:32 +0200

    Add getClassnames helper and use it on Heading2, Paragraph

--- src/components/Heading2/index.js
+++ src/components/Heading2/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './heading2.scss';
+import getClassnames from 'helpers/getClassnames';
 
-const Heading2 = ({ children, text }) => (
-  <h2 className={styles.root}>
+const Heading2 = ({ children, text, size }) => (
+  <h2 className={getClassnames(styles, ['root', size])}>
     {children || text}
   </h2>
 );
--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './paragraph.scss';
+import getClassNames from 'helpers/getClassNames';
 
-const Paragraph = ({ children, text, noMargin }) => (
-  <p className={styles[noMargin ? 'no-margin' : 'root']}>
+const Paragraph = ({ children, text, margin, size }) => (
+  <p className={getClassNames(styles, ['root', margin, size])}>
     {children || text}
   </p>
 );
