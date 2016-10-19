9c3e97a0c41139721371e038646cf4ab56476f62
Matt Berridge <hi@builtbyboon.com>
2016-10-19 22:29:04 +0200

    Misc refactors

--- src/containers/PreviewPetition.js
+++ src/containers/PreviewPetition.js
@@ -6,7 +6,6 @@ import { fetchPetition, publishPetition } from 'actions/PetitionActions';
 import settings from 'settings';
 import PreviewPetition from 'components/PreviewPetition';
 import getPetitionForm from 'selectors/petitionForm';
-import trustSubmitted from 'helpers/trustSubmitted';
 
 const PreviewPetitionContainer = withRouter(React.createClass({
   render () {
@@ -23,12 +22,8 @@ PreviewPetitionContainer.fetchData = ({ store, params }) => {
   return store.dispatch(fetchPetition(params.id));
 };
 
-export const mapStateToProps = ({ petition, trust, me }) => ({
-  petition: getPetitionForm(petition),
-  trustSubmitted: trustSubmitted(petition, trust),
-  isTrustedUser: trust.isTrustedUser,
-  isLoggedIn: me && !!me.id,
-  me
+export const mapStateToProps = ({ petition }) => ({
+  petition: getPetitionForm(petition)
 });
 
 export const mapDispatchToProps = (dispatch) => ({
