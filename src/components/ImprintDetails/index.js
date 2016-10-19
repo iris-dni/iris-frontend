72dc5b1260aba77bf69393d4c1106cd37fd70100
Eric Schäfer <omg@eric-schaefer.com>
2016-10-19 12:10:07 +0200

    Add map key

--- src/components/ImprintDetails/index.js
+++ src/components/ImprintDetails/index.js
@@ -15,13 +15,13 @@ const ImprintDetails = () => (
       <div className={styles.block}>
         <h3>{settings.imprint.detail.company.title}</h3>
         {Object.keys(settings.imprint.detail.company).slice(1).map(item =>
-          <div>{settings.imprint.detail.company[item]}</div>
+          <div key={item}>{settings.imprint.detail.company[item]}</div>
         )}
       </div>
       <div className={styles.block}>
         <h3>{settings.imprint.detail.contact.title}</h3>
         {Object.keys(settings.imprint.detail.contact).slice(1).map(item =>
-          <div>{settings.imprint.detail.contact[item]}</div>
+          <div key={item}>{settings.imprint.detail.contact[item]}</div>
         )}
       </div>
     </div>
