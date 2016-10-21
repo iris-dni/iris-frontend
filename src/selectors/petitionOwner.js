cd938168caed38e50431713a5f22122e4af1cd0d
Matt Berridge <hi@builtbyboon.com>
2016-10-21 12:58:16 +0200

    Cleaner function

--- src/selectors/petitionOwner.js
+++ src/selectors/petitionOwner.js
@@ -1,11 +1,12 @@
 import { get } from 'lodash/object';
 
 export default (petition = {}) => {
-  if (petition.owner) {
-    return [
-      get(petition, 'owner.firstname') || get(petition, 'owner.data.firstname'),
-      get(petition, 'owner.lastname') || get(petition, 'owner.data.lastname')
-    ].join(' ');
+  const firstname = get(petition, 'owner.firstname') || get(petition, 'owner.data.firstname');
+  const lastname = get(petition, 'owner.lastname') || get(petition, 'owner.data.lastname');
+
+  if (petition.owner && (firstname || lastname)) {
+    return [firstname, lastname].join(' ');
   }
+
   return '';
 };
