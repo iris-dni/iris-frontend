f69514e8b724e0936880303c31c3dd911155b551
Eric Schäfer <omg@eric-schaefer.com>
2016-10-26 12:49:23 +0200

    Removing title length limit

--- src/components/WidgetTitle/index.js
+++ src/components/WidgetTitle/index.js
@@ -3,10 +3,8 @@ import styles from './widget-title.scss';
 import Heading1 from 'components/Heading1';
 
 const WidgetTitle = ({ title }) => (
-  <div className={styles.ellipsis}>
-    <div className={styles.title}>
-      <Heading1 text={title} />
-    </div>
+  <div className={styles.title}>
+    <Heading1 text={title} />
   </div>
 );
 
