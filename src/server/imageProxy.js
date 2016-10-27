1b62b442443bac9f9246c3353b266c0c87eaf90d
Matt Berridge <hi@builtbyboon.com>
2016-10-27 15:31:12 +0200

    Adds proxy image signing

--- src/server/imageProxy.js
+++ src/server/imageProxy.js
@@ -1,4 +1,35 @@
+import crypto from 'crypto';
+import encodeParams from 'helpers/encodeParams';
+import { omit } from 'lodash/object';
+
 export default (request, reply) => {
-  // const { params, query } = request;
-  reply.redirect('http://google.co.uk').code(301);
+  const hmac = crypto.createHmac('sha1', process.env.IMAGE_PROXY_KEY);
+  const { query } = request;
+  const domain = query.domain;
+
+  // Create query string, removed path
+  const queryString = omit(query, 'domain');
+  const encodedQueryString = encodeParams(queryString);
+
+  // Return 404 if if no domain or url params are given
+  if (!query.domain || query.url) {
+    return reply('Image not found: please specify domain and url as params').code(404);
+  }
+
+  // Warn ion console if no dimensions given
+  if (isNaN(query.w) && isNaN(query.h)) {
+    console.warn('You must specify at least one dimension param for image', query.url); // eslint-disable-line no-console
+  }
+
+  // Create image signature
+  hmac.write(encodedQueryString);
+  hmac.end();
+  const imageSignature = hmac.read().hexSlice();
+
+  // Join query string with signature
+  const newQuery = Object.assign({}, queryString, { sig: imageSignature });
+
+  // Create full signed image url and redirect
+  const signedImage = [domain, encodeParams(newQuery)].join('?');
+  reply.redirect(signedImage).code(301);
 };
