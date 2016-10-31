4859d1f2a223a1198e5ca86d9d548eb91412eae0
Matt Berridge <hi@builtbyboon.com>
2016-10-31 15:45:17 +0100

    Remove image placeholder, tweak title font size

--- src/widgets/components/PetitionWidget/index.js
+++ src/widgets/components/PetitionWidget/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './petition-widget.scss';
 import WidgetBranding from 'widgets/components/WidgetBranding';
-import PetitionWidgetImage from 'widgets/components/PetitionWidgetImage';
+// import PetitionWidgetImage from 'widgets/components/PetitionWidgetImage';
 import PetitionWidgetByline from 'widgets/components/PetitionWidgetByline';
 import PetitionWidgetTitle from 'widgets/components/PetitionWidgetTitle';
 import PetitionWidgetProgress from 'widgets/components/PetitionWidgetProgress';
@@ -18,13 +18,13 @@ const PetitionWidget = ({
   progress
 }) => (
   <article className={styles.root}>
-    <div className={styles.upper}>
+    {/* <div className={styles.upper}>
       <PetitionWidgetImage
         alt={title}
         link={link}
         src={'http://placehold.it/800x450'}
       />
-    </div>
+    </div> */}
     <div className={styles.content}>
       <PetitionWidgetByline text={byline} />
       <PetitionWidgetTitle
