e11be89f7b0d77dac1aeb29b85510fd823c24f80
Matt Berridge <hi@builtbyboon.com>
2016-11-01 15:27:10 +0100

    Copy

--- src/settings/config.js
+++ src/settings/config.js
@@ -211,7 +211,7 @@ export default {
       invalidSimilarLink: 'You can only add unique links'
     },
     image: {
-      label: 'Cover image',
+      label: 'Add a cover image',
       hint: 'Add an image to make your petition more appealing. Landscape images are recommended and your image must a JPEG, GIF or PNG, maximum 2MB. ',
       placeholder: 'Upload image',
       invalidFileError: 'One or more files were invalid and could not be uploaded'
