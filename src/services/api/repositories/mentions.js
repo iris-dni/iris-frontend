dc73b163fe2fb517b911265329e03a4d6ff3fea7
Richard Bausek <r.bausek@de.edenspiekermann.com>
2016-11-02 02:07:27 +0100

    refactor mentions service

--- src/services/api/repositories/mentions.js
+++ src/services/api/repositories/mentions.js
@@ -1,5 +1,6 @@
 // Service for tracking widget embeds on the server
 // Note: this should not be part of any frontend bundle
+// to not to expose the API key
 
 import ApiClient, { GET } from 'services/api/client';
 import path from 'path';
@@ -8,12 +9,12 @@ const API_KEY = process.env.API_KEY;
 
 export default {
   track: (id, url) => {
-    const requestPath = path.join('/petition', id.toString(), '/mentions');
+    const requestPath = path.join('/petition', id.toString(), 'mentions');
     const requestParams = {url: url};
     const requestHeader = {
       'X-IRIS-APIKEY': API_KEY
     };
-    // TODO: enhance client to pass on request headers
+
     return ApiClient.request(requestPath, requestParams, GET, requestHeader);
   }
 };
