446c37a8b864d28270644f757724a82db765633f
Matt Berridge <hi@builtbyboon.com>
2016-11-03 11:48:52 +0100

    Small styling tweaks

--- src/components/IconAndInfo/index.js
+++ src/components/IconAndInfo/index.js
@@ -3,7 +3,7 @@ import Icon from 'components/Icon';
 import styles from './icon-and-info.scss';
 
 const IconAndInfo = ({ children, icon, info, size }) => (
-  <span>
+  <span className={styles.root}>
     <span className={styles.icon}>
       <Icon id={icon} size={size} inline />
     </span>
