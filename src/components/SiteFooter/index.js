e9635808a5f76152ad24dbb36818a6e01cd55dd6
Matt Berridge <hi@builtbyboon.com>
2016-11-10 17:53:48 +0100

    Fix include

--- src/components/SiteFooter/index.js
+++ src/components/SiteFooter/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './site-footer.scss';
 import settings from 'settings';
 import CallToAction from 'components/CallToAction';
-import SiteCopyright from 'components/Container';
+import SiteCopyright from 'components/SiteCopyright';
 
 const showFooterCTA = (section) => {
   switch (section) {
