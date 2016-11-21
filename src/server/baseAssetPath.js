5d29104ee6b9270bf7093cf6f7c098d4e7a93764
Matt Berridge <hi@builtbyboon.com>
2016-11-21 12:29:54 +0100

    Fix sticky footer browser bugs

--- src/server/baseAssetPath.js
+++ src/server/baseAssetPath.js
@@ -1,4 +1,4 @@
 export default [
-  process.env.NODE_ENV === 'production' ? '' : '//localhost:8080',
+  process.env.NODE_ENV === 'production' ? '' : '//dhcp133.de.edenspiekermann.com:8080',
   'dist'
 ].join('/');
