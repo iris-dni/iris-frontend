2f161efd5a3fef07742ba947f82c63148ef06cd0
Matt Berridge <hi@builtbyboon.com>
2016-11-22 14:23:55 +0100

    dev workflow

--- src/server/baseAssetPath.js
+++ src/server/baseAssetPath.js
@@ -1,6 +1,5 @@
+import devServer from '../../dev-server';
+
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default [
-  !isProduction ? '//localhost:8080' : '',
-  'dist'
-].join('/');
+export default isProduction ? '/dist' : devServer.publicPath;
