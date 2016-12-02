463a34e3645f244016059f741f09522ebb9454c7
Matt Berridge <hi@builtbyboon.com>
2016-12-02 12:35:14 +0100

    Remove braces

--- src/server/stringifyHeadData.js
+++ src/server/stringifyHeadData.js
@@ -1,6 +1,6 @@
 export default (helmetData = {}) => ({
   title: helmetData.title && helmetData.title.toString(),
-  meta: (helmetData.meta && helmetData.meta.toString()),
+  meta: helmetData.meta && helmetData.meta.toString(),
   link: helmetData.link && helmetData.link.toString(),
   script: helmetData.script && helmetData.script.toString(),
   style: helmetData.style && helmetData.style.toString()
