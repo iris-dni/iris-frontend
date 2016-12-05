75dc5a8ce4c0a5fb0beb3e2005301fceaca59390
Matt Berridge <hi@builtbyboon.com>
2016-12-05 16:50:44 +0100

    Add padding top generation to image container

--- src/components/ImageContainer/index.js
+++ src/components/ImageContainer/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import styles from './image-container.scss';
 import Image from 'components/Image';
 
+const getPaddingForImage = (attrs = {}) => (attrs.h / attrs.w) * 100 + '%';
+
 const ImageContainer = (props) => (
-  <div className={styles.root}>
+  <div className={styles.root} style={{ paddingTop: getPaddingForImage(props.attrs) }}>
     <div className={styles.inner}>
       <Image {...props} />
     </div>
