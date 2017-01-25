2420a9eac85a00ae8862236615d05e1055a7f6be
Nico Durstewitz <n.durstewitz@de.edenspiekermann.com>
2017-01-25 18:26:10 +0100

    Skip img srcSet and sizes if given srcSet is empty

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './image.scss';
 import createSignedImageUrl from 'helpers/createSignedImageUrl';
-import createSrcSet from 'helpers/createSrcSet';
+import createResponsiveImageAttributes from 'helpers/createResponsiveImageAttributes';
 
 const Image = ({ src, srcSet = [], attrs, sizes, alt }) => (
   <img className={styles.root}
     src={attrs ? createSignedImageUrl(src, attrs) : src}
-    srcSet={srcSet.length ? createSrcSet(src, attrs, srcSet) : ''}
-    sizes={sizes || ''}
     alt={alt || ''}
+
+    {...createResponsiveImageAttributes(src, srcSet, attrs, sizes)}
   />
 );
 
