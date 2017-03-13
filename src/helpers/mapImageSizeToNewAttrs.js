063a380403e1366fc801a350369bbbbfbafd6133
Benno Meysmans <Benno_njord@hotmail.com>
2017-03-13 18:15:45 +0100

    Fix decimal image with on request

--- src/helpers/mapImageSizeToNewAttrs.js
+++ src/helpers/mapImageSizeToNewAttrs.js
@@ -1,10 +1,11 @@
 import { get } from 'lodash/object';
+import { round } from 'lodash/math';
 
 export default (size = 0, attrs = {}) => (
   size ? {
     w: parseInt(size, 10),
     h: get(attrs, 'h', attrs.height)
-      ? (parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10)
+      ? round(parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10)
       : ''
   } : {}
 );
