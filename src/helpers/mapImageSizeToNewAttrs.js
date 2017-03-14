e89e8d416a0740074ecf9e9105533e910ddc5719
Benno Meysmans <Benno_njord@hotmail.com>
2017-03-14 10:49:30 +0100

    Add calculateImageHeight to helper

--- src/helpers/mapImageSizeToNewAttrs.js
+++ src/helpers/mapImageSizeToNewAttrs.js
@@ -4,8 +4,12 @@ import { round } from 'lodash/math';
 export default (size = 0, attrs = {}) => (
   size ? {
     w: parseInt(size, 10),
-    h: get(attrs, 'h', attrs.height)
-      ? round(parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10)
-      : ''
+    h: calculateImageHeight(size, attrs)
   } : {}
+);
+
+const calculateImageHeight = (size = 0, attrs = {}) => (
+  get(attrs, 'h', attrs.height)
+    ? round((parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10))
+    : ''
 );
