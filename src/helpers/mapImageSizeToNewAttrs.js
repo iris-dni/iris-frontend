24153f2e88b649ea6289bcd5dfa7e576f91a9326
Benno Meysmans <Benno_njord@hotmail.com>
2017-03-16 14:20:20 +0100

    Extract calculateImageHeight to own helper

--- src/helpers/mapImageSizeToNewAttrs.js
+++ src/helpers/mapImageSizeToNewAttrs.js
@@ -1,15 +1,8 @@
-import { get } from 'lodash/object';
-import { round } from 'lodash/math';
+import calculateImageHeight from 'helpers/calculateImageHeight';
 
 export default (size = 0, attrs = {}) => (
   size ? {
     w: parseInt(size, 10),
     h: calculateImageHeight(size, attrs)
   } : {}
-);
-
-const calculateImageHeight = (size = 0, attrs = {}) => (
-  get(attrs, 'h', attrs.height)
-    ? round((parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10))
-    : ''
 );
