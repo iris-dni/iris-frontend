4b76e925c8b0a722041ea4c6b0d4d1fa8216718f
Benno Meysmans <Benno_njord@hotmail.com>
2017-03-16 14:20:20 +0100

    Extract calculateImageHeight to own helper

---
+++ src/helpers/calculateImageHeight.js
@@ -0,0 +1,8 @@
+import { round } from 'lodash/math';
+import { get } from 'lodash/object';
+
+export default (size = 0, attrs = {}) => (
+  get(attrs, 'h', attrs.height)
+    ? round((parseInt(size, 10) / parseInt(get(attrs, 'w', attrs.width), 10)) * parseInt(get(attrs, 'h', attrs.height), 10))
+    : ''
+);
