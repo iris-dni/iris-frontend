ec644f57e7e5c11de99b46e11e5d79786882cb33
Benno Meysmans <Benno_njord@hotmail.com>
2017-04-04 17:23:51 +0200

    Add todo comment for copywriting

--- src/theme/translations.js
+++ src/theme/translations.js
@@ -37,6 +37,7 @@ export default {
         "label": "Select a city",
         "placeholder": "Enter name or ZIP"
       },
+      // TODO: Add proper copywriting
       "waiting": "Waiting petitions",
       "answered": "Answered petitions",
       "winning": "Winning petitions",
