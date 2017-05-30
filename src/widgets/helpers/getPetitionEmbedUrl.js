bd883dd855fe646d54df190f2f83d36c17c0851d
Lukas Ender <hello@lukasender.at>
2017-05-30 10:48:10 +0200

    Embed Widget: Use 'canonical' as referrer, if present

--- src/widgets/helpers/getPetitionEmbedUrl.js
+++ src/widgets/helpers/getPetitionEmbedUrl.js
@@ -1,7 +1,6 @@
 import baseUrl from 'helpers/baseUrl';
 
-export default (id) => {
-  const referrer = window.location.href;
-
-  return baseUrl(`embed/${id}?ref=${referrer}`);
+export default (id, referrer = null) => {
+  const refUrl = referrer || window.location.href;
+  return baseUrl(`embed/${id}?ref=${refUrl}`);
 };
